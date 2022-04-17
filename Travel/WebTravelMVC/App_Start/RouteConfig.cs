3fac42003abf4c1c032f667b855eca701bc742b7
Phan Định <81685801+phanvandinh99@users.noreply.github.com>
2022-04-17 16:10:14 +0700

    Add: project

--- Travel/WebTravelMVC/App_Start/RouteConfig.cs
+++ Travel/WebTravelMVC/App_Start/RouteConfig.cs
@@ -12,12 +12,12 @@ namespace WebTravelMVC
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            name: "Default",
+            url: "{controller}/{action}/{id}",
+            defaults: new { Controller = "Home", action = "Index", id = UrlParameter.Optional },
+            namespaces: new[] { "WebTravelMVC.Areas.Common.Controllers" }
+        ).DataTokens.Add("Area", "Common");
         }
     }
 }
---
+++ Travel/WebTravelMVC/Areas/Common/Controllers/PartialviewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebTravelMVC.Models;
+
+namespace WebTravelMVC.Areas.Common.Controllers
+{
+    public class PartialviewController : Controller
+    {
+        EFTravel db = new EFTravel();
+        public ActionResult TheLoai_Par(int iMaDanhMuc)
+        {
+            var theLoai = db.TheLoais.Where(n => n.MaDanhMuc == iMaDanhMuc).ToList();
+            return PartialView(theLoai);
+        }
+
+        // Bài viết mới
+        public ActionResult BaiVietMoi_Par()
+        {
+            var baiViet = db.BaiViets.OrderByDescending(n => n.MaBaiViet).ToList().Take(4);
+            return PartialView(baiViet);
+        }
+    }
+}
